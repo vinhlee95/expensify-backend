67fe96cff3eeec02376f5f703f9de6d9a125be7f
Vinh Le <vinh@vertics.co>
2019-08-11 13:51:27 +0300

    feat: add api and service test for get teams by user id - Update test snapshot to have team read permission

--- src/tests/api/team/api.test.ts
+++ src/tests/api/team/api.test.ts
@@ -4,14 +4,15 @@ import faker from 'faker'
 
 import {getRoleWithPermisison, signInUser, apiRequest} from '../../utils/common'
 import {Permission} from '../../../middlewares/permission'
-import {addUser} from '../../utils/db'
+import {addUser, addTeam} from '../../utils/db'
 import {createMockUser, createMockTeam} from '../../utils/mock'
 import {UserRole, UserStatus} from '../../../resources/user/user.interface'
 import {UserDocument} from '../../../resources/user/user.model'
 import {ErrorCode} from '../../../utils/apiError'
 
 describe('[TEAMS API]', () => {
-	const roleWithWriteUser = getRoleWithPermisison(Permission.WriteUser)
+	const roleWithWriteTeam = getRoleWithPermisison(Permission.WriteTeam)
+	const roleWithReadTeam = getRoleWithPermisison(Permission.ReadTeam)
 
 	let user: UserDocument
 	let token: string
@@ -21,8 +22,34 @@ describe('[TEAMS API]', () => {
 		token = signInUser(user)
 	})
 
+	describe('GET /api/teams', () => {
+		it(`[${roleWithReadTeam}]. should return 400 user id is not provided as a param`, async () => {
+			// Action
+			const result = await apiRequest
+				.get('/api/teams')
+				.set('Authorization', token)
+			// Expect
+			expect(result.status).toEqual(httpStatus.BAD_REQUEST)
+		})
+	})
+
+	describe('GET /api/teams', () => {
+		it(`[${roleWithReadTeam}]. should return 200 with teams that user belongs to`, async () => {
+			// Arrange
+			await addTeam(createMockTeam(user._id))
+			// Action
+			const result = await apiRequest
+				.get('/api/teams')
+				.set('Authorization', token)
+				.query({userId: user._id.toString()})
+
+			// Expect
+			expect(result.status).toEqual(httpStatus.OK)
+		})
+	})
+
 	describe('POST /api/teams', () => {
-		it(`[${roleWithWriteUser}]. should return 201 with new created team`, async () => {
+		it(`[${roleWithWriteTeam}]. should return 201 with new created team`, async () => {
 			// Arrange
 			const teamData = createMockTeam()
 
@@ -37,7 +64,7 @@ describe('[TEAMS API]', () => {
 			expect(result.body.data.name).toEqual(teamData.name)
 		})
 
-		it(`[${roleWithWriteUser}]. should return 400 when team name is neither provided nor a string`, async () => {
+		it(`[${roleWithWriteTeam}]. should return 400 when team name is neither provided nor a string`, async () => {
 			// Arrange
 			const noNameData = {}
 			const nonStringNameData = {name: faker.random.number}
--- src/tests/api/team/service.test.ts
+++ src/tests/api/team/service.test.ts
@@ -1,6 +1,6 @@
 import {addUser} from '../../utils/db'
 import {createMockTeam, createMockUser} from '../../utils/mock'
-import {createOne} from '../../../resources/team/team.service'
+import {createOne, getByUserId} from '../../../resources/team/team.service'
 import {UserDocument} from '../../../resources/user/user.model'
 import {UserRole, UserStatus} from '../../../resources/user/user.interface'
 import {Team} from '../../../resources/team/team.interface'
@@ -24,6 +24,21 @@ describe('[Team service]', () => {
 				// Expect
 				expect(createdTeam.name).toEqual(team.name)
 				expect(createdTeam.creatorId).toEqual(user._id.toString())
+			} catch (e) {
+				expect(e).toBeUndefined()
+			}
+		})
+	})
+
+	describe('getByUserId', () => {
+		it('should return all teams that user belongs to', async () => {
+			try {
+				// Action
+				await createOne(team, user._id)
+				const teams = await getByUserId(user._id)
+
+				// Expect
+				expect(teams.length).toEqual(1)
 			} catch (e) {
 				expect(e).toBeUndefined()
 			}
