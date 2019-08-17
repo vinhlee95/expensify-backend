b83eb088425222594d465b9bd8a2298496b119fc
Vinh Le <vinh@vertics.co>
2019-08-17 14:01:56 +0300

    feat: save slug in pre-save and add generic find by slug

--- src/resources/team/team.model.ts
+++ src/resources/team/team.model.ts
@@ -1,8 +1,10 @@
 import mongoose, {Document, Schema} from 'mongoose'
-
 import {Team} from './team.interface'
+import {slugify} from '../../utils/util'
 
-export interface TeamDocument extends Document, Team {}
+export interface TeamDocument extends Document, Team {
+	findBySlug: () => any
+}
 
 const teamSchema = new Schema(
 	{
@@ -18,9 +20,24 @@ const teamSchema = new Schema(
 			ref: 'user',
 			required: true,
 		},
+		slug: {
+			type: String,
+		},
 	},
 	{timestamps: true},
 )
+
+teamSchema.pre<TeamDocument>('save', function(next) {
+	if (this.isModified('name')) {
+		this.slug = slugify(this.name)
+	}
+
+	return next()
+})
+
+teamSchema.methods.findBySlug = function(slug: string) {
+	return this.find({slug})
+}
 
 const TeamModel = mongoose.model<TeamDocument>('team', teamSchema)
 
